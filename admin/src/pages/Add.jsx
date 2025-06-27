94a9de507759122f86f23e33f27b9835e8e4b8de
Yashasvi Sharma <yashharit1991@gmail.com>
2025-06-27 11:22:13 +0530

    Update Add.jsx

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 import axios from 'axios'
 import { backendUrl } from '../App'
 import { toast } from 'react-toastify'
-import '../styles/Add.css'
+import '../Styles/Add.css'
 
 const Add = ({ token }) => {
   const [image1, setImage1] = useState(false)
